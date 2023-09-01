855bca411bb695c8a5115c7986c14877a278a599
nahyeon99 <nahyeonee99@gmail.com>
2023-09-01 22:03:31 +0900

    [Init] project set up

---
+++ src/main/java/com/mapping/notionpage/NotionpageApplication.java
@@ -0,0 +1,13 @@
+package com.mapping.notionpage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotionpageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotionpageApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mapping/notionpage/NotionpageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mapping.notionpage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotionpageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
